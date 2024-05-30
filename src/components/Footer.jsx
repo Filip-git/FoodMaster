590c0e518c8f465c3725e849bdbf43523a248559
Filip Oroz <filip.oroz9@gmail.com>
2024-05-30 20:51:17 +0200

    logo update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/photos/FoodMaster.png'
 
 const Footer = () => {
   const currentDate = new Date().getFullYear();
@@ -9,7 +10,7 @@ const Footer = () => {
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link to="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="FoodMaster Logo" />
+          <img className="h-12" src={logo} alt="FoodMaster Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FoodMaster</span>
           </Link>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
