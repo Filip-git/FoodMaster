375b97fe0fb7788a216844780dca0026c17c833f
Filip Oroz <filip.oroz9@gmail.com>
2024-09-28 19:36:17 +0200

    init commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -90,12 +90,12 @@ useEffect(() => {
                     <li>
                       <Link to="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</Link>
                     </li>
-                    <li>
+                    {/* <li>
                       <Link to="/settings" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</Link>
                     </li>
                     <li>
                       <Link to="/saved-recipes" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Saved Recipes</Link>
-                    </li>
+                    </li> */}
                     <li>
                       <button onClick={handleLogout} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</button>
                     </li>
