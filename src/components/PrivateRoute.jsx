854148a0fad6924d463ab8ccf604579b99c66dac
Filip Oroz <filip.oroz9@gmail.com>
2024-10-04 21:09:52 +0200

    small fix

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import './Spinner.css';
 
 const PrivateRoute = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const auth = getAuth();
-  const isAuthenticated = auth.currentUser != null;
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setLoading(false);  // Stop loading once we know the authentication state
+    });
+
+    // Clean up the subscription
+    return () => unsubscribe();
+  }, [auth]);
+
+  if (loading) {
+    return (
+      <div className="spinner-container">
+        <div className="spinner"></div>
+      </div>
+    );
+  }
+
+  // Redirect to login if not authenticated
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
